9ff4bb8f240984992be5bd46c06a8d327608a5e6
HighExecutor <1123581321qwe>
2019-12-12 20:33:34 +0300

    test set to 1 launch, printing of episodes progress

--- episode.py
+++ episode.py
@@ -129,6 +129,8 @@ def run_episode_not_parallel(ei, args):
     reward, sars_list = episode(ei, config, test_wfs, test_size)
     remember(sars_list)
     replay(config['batch_size'])
+    if ei % 100 == 0:
+        print("episode {} completed".format(ei))
     return reward
 
 
@@ -138,6 +140,8 @@ def run_episode_lstm(ei, args):
     reward, sars_list = episode_lstm(ei, config, test_wfs, test_size)
     remember(sars_list)
     replay(config['batch_size'])
+    if ei % 100 == 0:
+        print("episode lstm {} completed".format(ei))
     return reward
 
 
@@ -146,28 +150,23 @@ def test_agent(args):
     config = parameter_setup(args, DEFAULT_CONFIG)
     test_wfs, test_times, test_scores, test_size = wf_setup(config['wfs_name'])
     for i in range(test_size):
-        best_saves = []
-        for k in range(5):
-            ttree, tdata, trun_times = test_wfs[i]
-            wfl = ctx.Context(config['agent_task'], config['nodes'], trun_times, ttree, tdata)
-            wfl.name = config['wfs_name'][i]
+        ttree, tdata, trun_times = test_wfs[i]
+        wfl = ctx.Context(config['agent_task'], config['nodes'], trun_times, ttree, tdata)
+        wfl.name = config['wfs_name'][i]
+        state = list(map(float, list(wfl.state)))
+        for time in range(wfl.n):
+            mask = list(map(int, list(wfl.get_mask())))
+            action = requests.post(f'{URL}test', json={'state': state, 'mask': mask, 'sched': False}).json()['action']
+            act_t, act_n = wfl.actions[action]
+            reward, wf_time = wfl.make_action(act_t, act_n)
+            next_state = list(map(float, list(wfl.state)))
             done = wfl.completed
-            state = list(map(float, list(wfl.state)))
-            for time in range(wfl.n):
-                mask = list(map(int, list(wfl.get_mask())))
-                action = requests.post(f'{URL}test', json={'state': state, 'mask': mask, 'sched': False}).json()['action']
-                act_t, act_n = wfl.actions[action]
-                reward, wf_time = wfl.make_action(act_t, act_n)
-                next_state = list(map(float, list(wfl.state)))
-                done = wfl.completed
-                state = next_state
-                if done:
-                    test_scores[i].append(reward)
-                    test_times[i].append(wf_time)
-                    best_saves.append((wfl, wf_time))
-                    break
-        best_saves = sorted(best_saves, key=lambda x: x[1])
-        write_schedule(i, best_saves[0][0])
+            state = next_state
+            if done:
+                test_scores[i].append(reward)
+                test_times[i].append(wf_time)
+                break
+        write_schedule(i, wfl)
 
 
 def test_agent_lstm(args):
@@ -175,33 +174,28 @@ def test_agent_lstm(args):
     config = parameter_setup(args, DEFAULT_CONFIG)
     test_wfs, test_times, test_scores, test_size = wf_setup(config['wfs_name'])
     for i in range(test_size):
-        best_saves = []
-        for k in range(5):
-            ttree, tdata, trun_times = test_wfs[i]
-            wfl = ctx.Context(config['agent_task'], config['nodes'], trun_times, ttree, tdata)
-            wfl.name = config['wfs_name'][i]
-            deq = LSTMDeque(seq_size=config['seq_size'], size=config['state_size'])
+        ttree, tdata, trun_times = test_wfs[i]
+        wfl = ctx.Context(config['agent_task'], config['nodes'], trun_times, ttree, tdata)
+        wfl.name = config['wfs_name'][i]
+        deq = LSTMDeque(seq_size=config['seq_size'], size=config['state_size'])
+        done = wfl.completed
+        state = list(map(float, list(wfl.state)))
+        deq.push(state)
+        state = deq.show()
+        for time in range(wfl.n):
+            mask = list(map(int, list(wfl.get_mask())))
+            action = requests.post(f'{URL}test', json={'state': state.tolist(), 'mask': mask, 'sched': False}).json()['action']
+            act_t, act_n = wfl.actions[action]
+            reward, wf_time = wfl.make_action(act_t, act_n)
+            next_state = list(map(float, list(wfl.state)))
+            deq.push(next_state)
+            next_state = deq.show()
             done = wfl.completed
-            state = list(map(float, list(wfl.state)))
-            deq.push(state)
-            state = deq.show()
-            for time in range(wfl.n):
-                mask = list(map(int, list(wfl.get_mask())))
-                action = requests.post(f'{URL}test', json={'state': state.tolist(), 'mask': mask, 'sched': False}).json()['action']
-                act_t, act_n = wfl.actions[action]
-                reward, wf_time = wfl.make_action(act_t, act_n)
-                next_state = list(map(float, list(wfl.state)))
-                deq.push(next_state)
-                next_state = deq.show()
-                done = wfl.completed
-                state = next_state
-                if done:
-                    test_scores[i].append(reward)
-                    test_times[i].append(wf_time)
-                    best_saves.append((wfl, wf_time))
-                    break
-        best_saves = sorted(best_saves, key=lambda x: x[1])
-        write_schedule(i, best_saves[0][0])
+            state = next_state
+            if done:
+                test_scores[i].append(reward)
+                test_times[i].append(wf_time)
+        write_schedule(i, wfl)
 
 
 def save():
